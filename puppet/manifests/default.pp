b33e0949a78e63f5bcbee584db379d4ebd8a8367
Kenny Gatdula <kennygatdula@gmail.com>
2015-09-27 13:34:43 -0400

    add ssh package

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -4,4 +4,21 @@ Exec {
 
 hiera_include('classes')
 
+package { 'openssh-server': ensure => 'installed' }
+
+service { 'sshd':
+            ensure  => 'running',
+            enable  => true,
+            require => Package['openssh-server']
+            }
+
+augeas { 'sshd_config':
+  context => '/files/etc/ssh/sshd_config',
+  changes => [
+    'set PrintMotd yes',
+  ],
+  require => Package['openssh-server'],
+  notify  => Service['sshd'],
+}
+
 notify{'in default.pp': }
